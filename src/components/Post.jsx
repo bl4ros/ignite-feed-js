070f2576cbf2557adc73d0a8ca1a988e25ceea81
Fernando <fernando.fnn97@gmail.com>
2023-02-06 23:06:17 -0300

    feat: learning react engines
    - add events and interactivity

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./Post.module.css";
 import { Comment } from "./Comment";
 import { Avatar } from "./Avatar";
@@ -5,6 +6,9 @@ import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = React.useState(["Post muito bacana ein?!"]);
+  const [newCommentText, setNewCommentText] = React.useState("");
+
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
@@ -15,6 +19,35 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  function handleNewCommentChange() {
+    event.target.setCustomValidity("");
+    setNewCommentText(event.target.value);
+  }
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("This field is invalid");
+  }
+
+  function deleteComment(comment) {
+    /* Nomear funções que serão executadas apartir de uma ação do usuário com on essa função será passada para outro component
+     */
+    const commentWithoutDeletedOne = comments.filter(
+      (commentCandidate) => comment !== commentCandidate
+    );
+
+    //imutabilidade -> as variáveis não sofrem mutação, nós criamos um novo valor (um novo espaço na memória)
+    setComments(commentWithoutDeletedOne);
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -28,7 +61,7 @@ export function Post({ author, publishedAt, content }) {
         </div>
         <time
           title={publishedDateFormatted}
-          dataTime={publishedAt.toISOString()}
+          dateTime={publishedAt.toISOString()}
         >
           {publishedDateRelativeToNow}
         </time>
@@ -37,10 +70,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -49,16 +82,33 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form className={styles.commentForm} onSubmit={handleCreateNewComment}>
         <strong>Deixe seu feedback</strong>
-        <textarea placeholder="Deixe um comentário" />
+        <textarea
+          name="comment"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+          placeholder="Deixe um comentário"
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
+        {comments?.map((comment) => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
+        })}
       </div>
     </article>
   );
